22bbc46cc229786bdb8c41d4b69deeac8acbaf79
dinesh-krishnamoorthy <49209753+dinesh-krishnamoorthy@users.noreply.github.com>
2019-04-02 20:12:24 -0400

    Create 2019JPCSOCESC.m

---
+++ _publications/2019JPCSOCESC.m
@@ -0,0 +1,18 @@
+---
+title: "On combining self-optimizing control and extremum-seeking control - Applied to an ammonia reactor case study"
+collection: publications
+permalink: /publication/2019JPCSOCESC
+excerpt: ''
+date: 2019-02-10
+venue: 'Journal of Process Control (ADCHEM Special issue invited paper)'
+paperurl: 'http://folk.ntnu.no/dineshk/Papers/2019/JPC_SOC_ESC_Preprint.pdf'
+citation: 'In-Press'
+---
+
+Abstract: 
+
+This paper proposes a primal decomposition algorithm for efficient computation of multistage scenario model predictive control, where the future evolution of uncertainty is represented by a scenario tree. This often results in large-scale optimization problems. Since the different scenarios are only coupled via the socalled non-anticipativity constraints, which ensures that the first control input is the same for all the scenarios, the different scenarios can be decomposed into smaller subproblems and solved iteratively using a master problem to coordinate the subproblems. We review the most common scenario decomposition methods and argue in favour of primal decomposition algorithms, since it ensures feasibility of the non-anticipativity constraints throughout the iterations which is crucial for closed-loop implementation. We also propose a novel backtracking algorithm to determine a suitable step length in the master problem to ensure the feasibility of the nonlinear constraints. The performance of the proposed approach and the backtracking algorithm is demonstrated using a CSTR case study
+
+[Download paper here](http://folk.ntnu.no/dineshk/Papers/2019/JPC_SOC_ESC_Preprint.pdf)
+
+Recommended citation: (In-Press).
